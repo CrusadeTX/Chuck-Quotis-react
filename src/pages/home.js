18b289593d1b3a823d3893d48e34d4e25ab57f2f
CrusadeTX <angel.terziev97@gmail.com>
2021-01-17 18:08:33 +0200

    Initial project set up

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+function Home(props){
+    return <>
+     <div className="container-lg container-fluid ">
+            <div className="row bg-danger shadow py-4">
+            <div className="col text-white">
+                <h1 className="text-center mb-0">Chuck Quotis</h1>
+                <div className="text-center">
+                    <span>The ultimate Chuck Norris Quotes collection</span>
+                </div> 
+            </div>
+        </div>
+    </div>
+    </>
+}
+export default Home;
